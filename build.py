a6f1951b11c2e3fb9875c3be7d2b0f923fbf5fa5
Tarmo Pikaro <tapika@yahoo.com>
2019-09-15 15:45:10 +0300

    try git clone with modifying files date / time

--- build.py
+++ build.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
-import os
-import sys
-import platform
+import os, sys, platform
+import subprocess, shlex
 
 isWindows = platform.system().lower().find("windows") != -1
 
@@ -19,11 +18,64 @@ if isWindows:
 
 
 print("OS: " + platform.system().lower())
-print("Current directory: " + os.getcwd() )
+#print("Current directory: " + os.getcwd() )
 
-print("environment variables:")
-for item, value in os.environ.items():
-    print('{}: {}'.format(item, value))
+#print("environment variables:")
+#for item, value in os.environ.items():
+#    print('{}: {}'.format(item, value))
+
+if os.utime in getattr(os, 'supports_follow_symlinks', []):
+    def lutime(path, times):
+        os.utime(path, times, follow_symlinks=False)
+else:
+    def lutime(path, times):
+        if not os.path.islink(path):
+            os.utime(path, times)
+
+def gitCloneOrUpdate(gitUrl, dir):
+    if not os.path.exists(dir):
+        os.system("git clone {} {}".format(gitUrl, dir))
+
+    filelist = set()
+
+    for root, subdirs, files in os.walk(dir):
+        for d in subdirs:
+            if(d == ".git" or d == "out"):
+                subdirs.remove(d);
+
+        for file in files:
+            filelist.add(os.path.relpath(os.path.join(root, file), dir))
+
+    os.chdir(dir)
+
+    mtime = 0
+    process = subprocess.Popen(shlex.split('git whatchanged --pretty=%at'), stdout=subprocess.PIPE)
+
+    for line in process.stdout:
+        line = line.strip()
+
+        # Blank line between Date and list of files
+        if not line: continue
+
+        # File line
+        if line.startswith(':'):
+            file = os.path.normpath(line.split('\t')[-1])
+            if file in filelist:
+                filelist.remove(file)
+                # print mtime, file
+                lutime(file, (mtime, mtime))
+
+        # Date line
+        else:
+            mtime = long(line)
+
+        # All files done?
+        if not filelist:
+            break
+
+
+gitCloneOrUpdate("https://github.com/tapika/cppreflect", "cppreflect")
+
 
 #if isWindows:
 #    exitCode=os.system("cmake . && cmake --build .")
