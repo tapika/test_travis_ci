ecc116a970d976b3244d9361259376f06c307a2a
Tarmo Pikaro <tapika@yahoo.com>
2019-09-19 07:28:18 +0300

    Disable multicpu option (does not work)

--- build.py
+++ build.py
@@ -57,7 +57,7 @@ if isWindows:
 cmd = cmd + ' "{}"'.format(projDir)
 execcmd(cmd)
 
-cmd='cmake -j 2 --build "{}" --config {}'.format(cachePath, buildType)
+cmd='cmake --build "{}" --config {}'.format(cachePath, buildType)
 if not execcmd(cmd, True, 25*60*60):    # 25 min
     print ("\nNote: Cancelled build, timeout\n")
 
